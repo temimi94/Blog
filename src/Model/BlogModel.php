74d2d3a4f86be770b3a0becbac57071dc780f847
Théo <kinder.theo@gmail.com>
2019-12-24 16:50:22 +0100

    Update BlogModel.php
    Create CreateComment query

--- src/Model/BlogModel.php
+++ src/Model/BlogModel.php
@@ -26,19 +26,23 @@ class BlogModel extends MainModel
         return $this->readAll('SELECT Comment.content, Comment.id_user, Comment.date, User.pseudo FROM Comment
         INNER JOIN Article ON Article.id_article = Comment.id_article
         INNER JOIN User ON Comment.id_user = User.id_user
-        WHERE Article.id_article = ' .$id_article);
+        WHERE Comment.validate = 1 AND Article.id_article = ' .$id_article);
 
     }
 
     public function createArticle($article_title, $article_content, $article_chapo, $author_id){
         $statement = 'INSERT INTO Article (title, content, date, chapo, author_id) VALUES (?, ?, ?, ?, ?)';
         $date = date("Y-m-d H:i:s");
-        if($author_id === 'Administrateur') $author_id = 1;
-        elseif($author_id === 'Utilisateur') $author_id = 2;
         $article = array($article_title, $article_content, $date, $article_chapo, $author_id);
-        return $this->add($statement, $article);
+        return $this->update($statement, $article);
     }
 
+    public function createComment($id_article, $content, $id_user){
+        $statement = 'INSERT INTO Comment (id_article, content, date, id_user) VALUES (?, ?, ?, ?)';
+        $date = date("Y-m-d H:i:s");
 
+        $comment = array($id_article, $content, $date, $id_user);
+        return $this->update($statement, $comment);
+    }
 
 }
