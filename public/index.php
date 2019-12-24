a4dc323ee770ccb4be5422c75c0bf30afc843a94
Théo <kinder.theo@gmail.com>
2019-12-24 14:15:57 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -6,12 +6,10 @@ use Tracy\Debugger;
 
 require_once '../vendor/autoload.php';
 
+
 Debugger::enable();
 
+
+
+
 new Router();
-/**
-$req = new \App\Model\BlogModel();
-echo '<pre>';
-var_dump($req->selectArticle(3));
-echo '</pre>';
- */
