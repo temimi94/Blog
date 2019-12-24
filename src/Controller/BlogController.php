24eafe4b85511ef383eae9c3804ecf386982da53
Théo <kinder.theo@gmail.com>
2019-12-24 16:49:43 +0100

    Update BlogController.php
    Create CreateCommentMethod

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -35,7 +35,7 @@ class BlogController extends MainController {
         if(array_search($id_blog, array_column($verif, 'id_article', $id_blog)) === false){
             header('Location: index.php?page=listblog');
             exit();
-        }
+        } // Create function ifArticleExist
         return $id_blog;
     }
 
@@ -46,9 +46,8 @@ class BlogController extends MainController {
         $article = new BlogModel();
 
         if(!empty($_POST)){
-            $post = array_map( 'htmlspecialchars', $_POST); /** Sécurise les données */
-
-            $article->createArticle($post['title'], $post['content'], $post['chapo'], $_SESSION['rank']);
+            $post = array_map( 'htmlspecialchars', $_POST); /** Sécurise les données */ //Create function securPost
+            $article->createArticle($post['title'], $post['content'], $post['chapo'], $_SESSION['id_user']);
             $this->redirect('listblog');
         }elseif(empty($_POST)){
             $view = $this->twig->render('createblog.twig');
@@ -56,12 +55,20 @@ class BlogController extends MainController {
         }
     }
 
-  /*  public function SendcommentMethod(){
-        if(!empty($_POST){
-            $_POST['email']
-            $_POST['message']
+    public function CreatecommentMethod(){
+        $post = array_map( 'htmlspecialchars', $_POST);
+        $id_blog = self::getId();
+        $sess = new SessionController();
+        $sess->isLegit();
+
+        if(!empty($post)){
+            $comment = new BlogModel();
+            $comment->createComment($id_blog, $post['comment'], $_SESSION['id_user']);
+            $this->redirect('blog&idblog='.$id_blog);
+        }elseif(empty($post)){
+            $this->redirect('blog&idblog=' .$id_blog);
         }
-    }*/
+    }
 
 
 
