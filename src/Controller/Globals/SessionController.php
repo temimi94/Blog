fd61435e35df9ff8be492ed5d50f922c1f15877c
Théo <kinder.theo@gmail.com>
2020-01-04 01:30:21 +0100

    Create Global/SessionController.php

---
+++ src/Controller/Globals/SessionController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Controller\Globals;
+
+class SessionController {
+
+    private $session;
+
+    private $user;
+
+    public function __construct(){
+        $this->session = filter_var_array($_SESSION);
+
+        if (isset($this->session['user'])) {
+            $this->user = $this->session['user'];
+        }
+
+    }
+
+    public function getUserArray()
+    {
+        return $this->user;
+    }
+
+    public function getUserVar($var)
+    {
+        return $this->user[$var];
+    }
+
+
+    public function getSessionArray(){
+        return $this->session;
+    }
+
+    public function getSessionVar($var){
+        return $this->session[$var];
+    }
+
+    public function setUserVar(string $var, $data){
+        $this->user[$var] = $data;
+    }
+
+    public function isLogged(){
+    if (!empty($this->session['session_id'])) {
+        return true;
+    } else {
+        return false;
+    }
+
+    }
+    public function createSession(array $data){
+        if($data['rank'] == 1) $data['rank'] = 'Administrateur';
+        elseif($data['rank'] == 2) $data['rank'] = 'Utilisateur';
+
+        $_SESSION['user'] = [
+            'session_id' => session_id(),
+            'pseudo' => $data['pseudo'],
+            'id_user' => $data['id_user'],
+            'email' => $data['email'],
+            'date_register' => $data['date_register'],
+            'rank' => $data['rank']
+        ];
+        $this->__construct(); /** Lance le constructeur pour mettre les données dans $this->session et $this->user */
+
+    }
+}
