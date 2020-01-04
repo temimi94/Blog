fd5d827ed18c306983bf6273bedfcd448833572c
Théo <kinder.theo@gmail.com>
2020-01-04 01:29:57 +0100

    Create globalController.php

---
+++ src/Controller/GlobalController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Controller\Globals\PostController;
+use App\Controller\Globals\GetController;
+use App\Controller\Globals\SessionController;
+
+abstract class GlobalController{
+
+    protected $post;
+
+    protected $get;
+
+    protected $session;
+
+    public function __construct()
+    {
+        $this->post = new PostController();
+        $this->get = new GetController();
+        $this->session = new SessionController();
+    }
+
+}
