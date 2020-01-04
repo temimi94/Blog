7ead5102b55d9ec6a549da127be459fb554e1ef9
Théo <kinder.theo@gmail.com>
2020-01-04 01:31:58 +0100

    Update UserController.php

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -13,8 +13,8 @@ class UserController extends MainController
     public function DefaultMethod(){
         $this->isLegitUser();
 
-        $main = new MainController;
-        return $main->twig->render('user.twig');
+
+        return $this->twig->render('user.twig');
     }
 
 
@@ -22,30 +22,29 @@ class UserController extends MainController
         $this->isLegitUser();
 
         $req = new UserModel();
-        $req = $req->selectArticleUser($_SESSION['id_user']);
+        $req = $req->selectArticleUser($this->session->getUserVar('id_user'));
 
-        $main = new MainController;
-        return $main->twig->render('user.twig', ['article' => $req]);
+        return $this->twig->render('user.twig', ['article' => $req]);
     }
 
     public function EditarticleMethod(){
         $this->isLegitUser();
-        $main = new MainController;
+        $post = $this->post->getPostArray();
 
-        if(!empty($_POST)) { /** Si $_POST existe et possède des données, les données sont ajoutées à la bdd */
+        if(!empty($post)) { /** Si $_POST existe et possède des données, les données sont ajoutées à la bdd */
             $edit = new UserModel();
-            $post = array_map( 'htmlspecialchars', $_POST); /** Sécurise les données */
+
             $edit->updateUserArticle($post['id_article'], $post['title'], $post['chapo'], $post['content']);
 
             $this->redirect('user&method=listarticle');
 
-        }elseif(empty($_POST)){ /**Si $_POST est vide, renvois sur formulaire pour saisir les données à changer **/
-            $get = filter_input(INPUT_GET, 'idarticle', FILTER_VALIDATE_INT);
+        }elseif(empty($post)){ /**Si $_POST est vide, renvois sur formulaire pour saisir les données à changer **/
+            $get = $this->get->getGetVar('idarticle');
             if($get === false) $this->redirect('user');
 
             $req = new BlogModel();
             $req = $req->selectArticle($get);
-            return $main->twig->render('userUpdateArticle.twig', ['article' => $req]);
+            return $this->twig->render('userUpdateArticle.twig', ['article' => $req]);
         }
     }
 
@@ -53,29 +52,27 @@ class UserController extends MainController
         $this->isLegitUser();
 
         $delete = new UserModel;
-        $delete->deleteUserArticle(filter_input(INPUT_GET, 'idarticle', FILTER_VALIDATE_INT), $_SESSION['id_user']);
+        $delete->deleteCommentByArticle($this->get->getGetVar('idarticle'));
+        $delete->deleteUserArticle($this->get->getGetVar('idarticle'), $this->session->getUserVar('id_user'));
 
-        $req = $delete->selectArticleUser($_SESSION['id_user']);
-//// Delete Comment Article
-        $main = new MainController;
-        return $main->twig->render('user.twig', ['article' => $req]);
+        $req = $delete->selectArticleUser($this->session->getUserVar('id_user'));
+        return $this->twig->render('user.twig', ['article' => $req]);
     }
 
     public function ListcommentMethod(){
         $this->isLegitUser();
 
         $req = new UserModel();
-        $req = $req->getUserComment($_SESSION['id_user']);
+        $req = $req->getUserComment($this->session->getUserVar('id_user'));
 
-        $main = new MainController;
-        return $main->twig->render('user.twig', ['comment' => $req]);
+        return $this->twig->render('user.twig', ['comment' => $req]);
     }
     
 
     public function DeletecommentMethod(){
         $this->isLegitUser();
 
-        $get = filter_input(INPUT_GET, 'idcomment', FILTER_VALIDATE_INT);
+        $get = $this->get->getGetVar('idcomment');
         if($get ===false) $this->redirect('user');
 
         $req = new UserModel();
@@ -85,7 +82,7 @@ class UserController extends MainController
 
 
     public function isLegitUser(){
-        if($_SESSION['rank'] != 'Utilisateur') $this->redirect('home');
+        if($this->session->getUserVar('rank') != 'Utilisateur') $this->redirect('home');
     }
 
 }
