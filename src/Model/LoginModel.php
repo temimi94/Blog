664fa5168c8f425e9845d56e0f4579775184f68c
Théo <kinder.theo@gmail.com>
2020-01-09 01:56:25 +0100

    update Models

--- src/Model/LoginModel.php
+++ src/Model/LoginModel.php
@@ -5,11 +5,13 @@ namespace App\Model;
 
 class LoginModel extends MainModel
 {
-    public function getUser($user_email){
-        return $this->read('SELECT * FROM User WHERE User.email ="'.$user_email.'"');
+    public function getUser($user_email)
+    {
+        return $this->read('SELECT * FROM User WHERE User.email ="' . $user_email . '"');
     }
 
-    public function createUser($pseudo, $email, $password){
+    public function createUser($pseudo, $email, $password)
+    {
         $statement = 'INSERT INTO User (pseudo, email, password, date_register) VALUES (?, ?, ?, ?)';
         $date = date("Y-m-d H:i:s");
 
--- src/Model/MainModel.php
+++ src/Model/MainModel.php
@@ -4,59 +4,66 @@
 namespace App\Model;
 
 
-class MainModel{
+class MainModel
+{
 
-    public function selectAllUser(){
+    public function selectAllUser()
+    {
         $database = ConnectPDO::getPDO()->prepare('SELECT * FROM User');
         $database->execute();
         return $req = $database->fetchAll();
     }
 
-    public static function selectAuthorById($author_id){
-        $database = ConnectPDO::getPDO()->prepare('SELECT * FROM User WHERE id_user = '.$author_id);
+    public static function selectAuthorById($author_id)
+    {
+        $database = ConnectPDO::getPDO()->prepare('SELECT * FROM User WHERE id_user = ' . $author_id);
         $database->execute();
         return $req = $database->fetch();
 
     }
 
-    public static function selectArticleWithAuthor(){
+    public static function selectArticleWithAuthor()
+    {
         $database = ConnectPDO::getPDO()->prepare('SELECT * FROM Article LEFT JOIN User ON Article.author_id = User.id_user');
         $database->execute();
         return $req = $database->fetch();
     }
 
 
-    public function read($statement){
+    public function read($statement)
+    {
         $req = ConnectPDO::getPDO()->prepare($statement);
         $req->execute();
         return $req->fetch();
     }
 
-    public function readAll($statement){
+    public function readAll($statement)
+    {
         $req = ConnectPDO::getPDO()->prepare($statement);
         $req->execute();
         return $req->fetchAll();
     }
 
-    public function delete($statement){
+    public function delete($statement)
+    {
         $req = ConnectPDO::getPDO()->prepare($statement);
         $req->execute();
         return $req;
     }
 
-    public function update($statement, $array = []){
+    public function update($statement, $array = [])
+    {
         $req = ConnectPDO::getPDO()->prepare($statement);
         $req->execute($array);
         return $req;
     }
 
-    public function add($statement){
+    public function add($statement)
+    {
         $req = ConnectPDO::getPDO()->prepare($statement);
         $req->execute();
         return $req;
     }
-
-
 
 
 }
