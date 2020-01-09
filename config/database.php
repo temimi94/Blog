4d1fe1fea9338dfaaf871dc9e1797a23f2f2a2ee
Théo <kinder.theo@gmail.com>
2020-01-10 00:17:51 +0100

    update database.php name

--- config/database.php
+++ config/database.php
@@ -2,8 +2,8 @@
 
 define('DB_DSN', 'mysql:host=localhost;dbname=p5;charset=UTF8');
 
-define('DB_USER', '');
+define('DB_USER', 'root');
 
-define('DB_PASS', '');
+define('DB_PASS', 'password');
 
 define('DB_OPTIONS', array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
