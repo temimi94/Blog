e307746a16b852cecbdf6705a6d83803e3aa3187
Théo <kinder.theo@gmail.com>
2020-01-10 00:20:39 +0100

    create setupMail.php

---
+++ config/setupMail.php
@@ -0,0 +1,6 @@
+<?php
+
+$configMail['smtp'] = ''; // l'adresse de votre serveur de mail
+$configMail['port'] = 25; // le port utilisé pour l'envoi des mails
+$configMail['username'] = ''; // l'adresse mail utilisée
+$configMail['password'] = ''; // le mot de passe de votre serveur de mail (ou de votre compte admin)
