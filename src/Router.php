fc16809876ccbdc78671f4b26d5b57e587238a78
Théo <kinder.theo@gmail.com>
2020-01-09 01:57:07 +0100

    update Router.php

--- src/Router.php
+++ src/Router.php
@@ -2,50 +2,55 @@
 
 namespace App;
 
-class Router{
+class Router
+{
 
     const DEFAULT_METHOD = 'DefaultMethod';
 
-    public function __construct(){
+    public function __construct()
+    {
         $this->parseUrl();
         $this->setController();
         $this->setMethod();
         $this->controllerSendView();
     }
-    public function parseUrl(){
+
+    public function parseUrl()
+    {
         $page = filter_input(INPUT_GET, 'page');
-        if(!isset($page)){
+        if (!isset($page)) {
             $page = 'home';
         }
         $this->controller = $page;
 
         $method = filter_input(INPUT_GET, 'method');
-        if(!isset($method)){
+        if (!isset($method)) {
             $method = 'DefaultMethod';
         }
         $this->method = $method;
     }
 
-    public function setController(){
+    public function setController()
+    {
         $this->controller = ucfirst(strtolower($this->controller));
         $this->controller = 'App\Controller\\' . $this->controller . 'Controller';
-        //var_dump($this->controller);
-        if(!class_exists($this->controller)){
+        if (!class_exists($this->controller)) {
             $this->controller = 'App\Controller\HomeController';
         }
-        //var_dump($this->controller);
 
     }
 
-    public function setMethod(){
-        $this->method = ucfirst(strtolower($this->method)) . 'Method';
+    public function setMethod()
+    {
+        $this->method = ucfirst($this->method) . 'Method';
 
-        if(!method_exists($this->controller, $this->method)){
-         $this->method = self::DEFAULT_METHOD;
+        if (!method_exists($this->controller, $this->method)) {
+            $this->method = self::DEFAULT_METHOD;
         }
     }
 
-    public function controllerSendView(){
+    public function controllerSendView()
+    {
         $this->controller = new $this->controller;
         $reponse = call_user_func([$this->controller, $this->method]);
 
