9fd5d1687cc9f68206fe046dcd05b9f9533d3642
Théo <kinder.theo@gmail.com>
2020-01-14 00:06:18 +0100

    Factory and Add PHP Doc in MainModel.php

--- src/Model/MainModel.php
+++ src/Model/MainModel.php
@@ -4,59 +4,56 @@
 namespace App\Model;
 
 
+/**
+ * Class MainModel
+ * @package App\Model
+ */
 class MainModel
 {
 
-    public static function selectAuthorById($author_id)
-    {
-        $database = ConnectPDO::getPDO()->prepare('SELECT * FROM User WHERE id_user = ' . $author_id);
-        $database->execute();
-        return $req = $database->fetch();
-
-    }
-
-    public static function selectArticleWithAuthor()
-    {
-        $database = ConnectPDO::getPDO()->prepare('SELECT * FROM Article LEFT JOIN User ON Article.author_id = User.id_user');
-        $database->execute();
-        return $req = $database->fetch();
-    }
-
-
-    public function read($statement)
+    /**
+     * @param $statement
+     * @return mixed
+     */
+    public function fetch($statement)
     {
         $req = ConnectPDO::getPDO()->prepare($statement);
         $req->execute();
         return $req->fetch();
     }
 
-    public function readAll($statement)
+    /**
+     * @param $statement
+     * @return array
+     */
+    public function fetchAll($statement)
     {
         $req = ConnectPDO::getPDO()->prepare($statement);
         $req->execute();
         return $req->fetchAll();
     }
 
-    public function delete($statement)
+    /**
+     * @param $statement
+     * @return bool|\PDOStatement
+     */
+    public function exec($statement)
     {
         $req = ConnectPDO::getPDO()->prepare($statement);
         $req->execute();
         return $req;
     }
 
-    public function update($statement, $array = [])
+    /**
+     * @param $statement
+     * @param array $array
+     * @return bool|\PDOStatement
+     */
+    public function execArray($statement, $array = [])
     {
         $req = ConnectPDO::getPDO()->prepare($statement);
         $req->execute($array);
         return $req;
     }
-
-    public function add($statement)
-    {
-        $req = ConnectPDO::getPDO()->prepare($statement);
-        $req->execute();
-        return $req;
-    }
-
 
 }
